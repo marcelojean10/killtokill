35a125da6694bcd793455b1d0afd6ccc287314d3
Marcelo Jean <marcelojeam1@gmail.com>
2019-08-12 21:04:51 -0300

    Alter variables and add spaces in conditionals

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,5 @@
 
-        var game = new Phaser.Game(800,600, Phaser.CANVAS, 'gameDiv'),
+        let game = new Phaser.Game(800,600, Phaser.CANVAS, 'gameDiv'),
             spacefield,
             backgroundv,
             player,
@@ -17,7 +17,7 @@
             pathImg = './assets/img/',
             pathSound = './assets/audio/';
 
-        var mainState = {
+        let mainState = {
 
             preload:function() {
                 game.load.image('spacefield', pathImg + "spacefield.jpg");
@@ -44,11 +44,11 @@
                 bullets.setAll('anchor.y', 1);
                 bullets.setAll('outOfBoundsKill', true);
                 bullets.setAll('checkWorldBounds', true);
-                //
-                audioBullet = game.add.audio('stars');//criando objeto audio
+
+                audioBullet = game.add.audio('stars');
                 audioBullet.loop = true;
                 audioBullet.volume = .5;
-                audioBullet.play();//assim q for criado, vai começar a musica
+                audioBullet.play();
 
                 fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
                 enemies = game.add.group();
@@ -58,18 +58,16 @@
                 createEnemies();
                 scoreText = game.add.text(0,550,'Score: ',{font:'32px Arial',fill: '#fff'});    
 
-                //text win game
                 winText = game.add.text(game.world.centerX - 100, game.world.centerY, 'Congratulations!\nYou Win!',{font:'32px emulogic', fill: '#fff'});
                 winText.visible = false;
 
-                //text end game
                 endText = game.add.text(game.world.centerX - 90, game.world.centerY - 50, 'You Lose!\nClick to restart', {font:'32px emulogic', fill: '#fff'});
                 endText.visible = false;
 
             },
 
             update:function() {
-                game.physics.arcade.overlap(bullets, enemies, collisionHandler,null,this);
+                game.physics.arcade.overlap(bullets, enemies, collisionHandler,null, this);
 
                 player.body.velocity.x = 0;
                 player.body.velocity.y = 0;
@@ -77,15 +75,14 @@
 
                 if(cursors.left.isDown) {
                     player.body.velocity.x -= 350;
-                    
                 }
+
                 if(cursors.right.isDown) {
                     player.body.velocity.x = 350;
                 }
 
                 if(cursors.up.isDown) {
                     player.body.velocity.y -= 350;
-
                 }
 
                 if(cursors.down.isDown) {
@@ -103,7 +100,7 @@
                     winText.visible = true;
                     scoreText.visible = false;
                     player.kill();
-                    //contar
+
                     game.time.events.loop(Phaser.Timer.SECOND, updateCounter, this);
                 }
             },
@@ -117,9 +114,9 @@
     }
     function fireBullet() {
         if(game.time.now > bulletTime) {
-            bullet = bullets.getFirstExists(false);
+            const bullet = bullets.getFirstExists(false);
 
-            if(bullet ){
+            if(bullet) {
                 bullet.reset(player.x + 14, player.y);
                 bullet.body.velocity.y -= 400;
                 bulletTime = game.time.now + 200;
@@ -128,9 +125,9 @@
     }
 
     function createEnemies() {
-        for(var y = 0; y < 4; y++) {
-            for(var x = 0; x < 10; x++) {
-                var enemy = enemies.create(x*48, y*50, 'enemy');
+        for(let y = 0; y < 4; y++) {
+            for(let x = 0; x < 10; x++) {
+                let enemy = enemies.create(x*48, y*50, 'enemy');
                 enemy.anchor.setTo(0.5,0.5);
             }
         }
@@ -138,17 +135,17 @@
         enemies.x = 100;
         enemies.y = 50;
 
-        var tween = game.add.tween(enemies).to({x:200}, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+        let tween = game.add.tween(enemies).to({x:200}, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
 
         tween.onLoop.add(descend, this);
     }
 
 
-    function descend(){
+    function descend() {
         enemies.y += 10;
     }
 
-    function collisionHandler(bullet, enemy){
+    function collisionHandler(bullet, enemy) {
         bullet.kill();
         enemy.kill();
 
